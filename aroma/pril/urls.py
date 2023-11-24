15137697947f0c686e474267a673548f6ff7a304
Симонов Алексей <alxxalxx@mail.ru>
2023-11-24 12:33:06 +0300

    rm unus urls

--- aroma/pril/urls.py
+++ aroma/pril/urls.py
@@ -6,7 +6,6 @@ app_name = 'pril'
 urlpatterns = [
     path('', views.index, name='index'),
     path('create/', views.Product_post, name='add_new'),
-    path('poison/', views.poison, name='poison'),
     path('planogram/', views.planogram, name='planogram_list'),
     path('planogram/edit/', views.planogram_edit, name='planogram_edit'),
 ]
